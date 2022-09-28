71f58086a9fbe1f778d2ceec0d042c76908e0a57
darren277 <darren277@yahoo.com>
2022-09-28 09:26:34 -0700

    Added tests for string length check and also table size max.

--- tests/main.py
+++ tests/main.py
@@ -22,11 +22,66 @@ def test_insert_and_select():
     p.stdin.flush()
     p.stdin.write(b'.exit\n')
     out = p.stdout.readlines()
+    print(out)
     assert out[0].rstrip() == 'db > Executed.'
     assert out[1].rstrip() == 'db > (1, cstack, foo@bar.com)'
     assert out[3].rstrip() == 'db > Syntax error. Could not parse statement.'
 
 
+def test_max_string_length():
+    long_username = "a"*32
+    long_email = "a"*255
+    print(long_username)
+
+    p = subprocess.Popen(['./main', 'mydb.db'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.stdin.write(b'insert 1 {} {}\n'.format(long_username, long_email))
+    p.stdin.flush()
+    p.stdin.write(b'select\n')
+    p.stdin.flush()
+    p.stdin.write(b'.exit\n')
+    out = p.stdout.readlines()
+    print(out)
+    assert out[1].rstrip() == 'db > (1, {}, {})'.format(long_username, long_email)
+
+    long_username = "a"*33
+    long_email = "a"*255
+    print(long_username)
+
+    p = subprocess.Popen(['./main', 'mydb.db'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.stdin.write(b'insert 1 {} {}\n'.format(long_username, long_email))
+    p.stdin.flush()
+    p.stdin.write(b'select\n')
+    p.stdin.flush()
+    p.stdin.write(b'.exit\n')
+    out = p.stdout.readlines()
+    print(out)
+    assert out[0].rstrip() == 'db > String is too long.'
+
+
+def test_table_full():
+    p = subprocess.Popen(['./main', 'mydb.db'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    for i in range(1400):
+        p.stdin.write(b'insert 1 cstack user@user.com\n')
+        p.stdin.flush()
+    p.stdin.write(b'.exit\n')
+    out = p.stdout.readlines()
+    print(out)
+    assert not any([x for x in out if 'Error: Table full' in x])
+
+    p = subprocess.Popen(['./main', 'mydb.db'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    for i in range(1401):
+        p.stdin.write(b'insert 1 cstack user@user.com\n')
+        p.stdin.flush()
+    p.stdin.write(b'.exit\n')
+    out = p.stdout.readlines()
+    print(out)
+    assert any([x for x in out if 'Error: Table full' in x])
+
+
 #test_main()
 test_insert_and_select()
+#test_table_full()
+
+test_max_string_length()
+
 print('All tests passed!')
