09eb65533dcac7c4c8a1b0d5b87cd49e799b5a45
darren277 <darren277@yahoo.com>
2022-09-29 06:53:45 -0700

    Added cursor for row traversal.

--- main.c
+++ main.c
@@ -18,13 +18,45 @@ typedef enum { PREPARE_SUCCESS, PREPARE_STRING_TOO_LONG, PREPARE_SYNTAX_ERROR, P
 typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
 
 
-// IN MEMORY READ/WRITE FOR A ROW
-void* row_slot(Table* table, uint32_t row_num) {
-  uint32_t page_num = row_num / ROWS_PER_PAGE;
-  void* page = get_page(table->pager, page_num);
-  uint32_t row_offset = row_num % ROWS_PER_PAGE;
-  uint32_t byte_offset = row_offset * ROW_SIZE;
-  return page + byte_offset;
+
+// CURSOR
+
+typedef struct {Table* table; uint32_t row_num; bool end_of_table;} Cursor;
+
+
+// UPDATED ROW_SLOT FUNCTION TO USE CURSOR
+Cursor* table_start(Table* table) {
+    Cursor* cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = (table->num_rows == 0);
+
+    return cursor;
+}
+
+Cursor* table_end(Table* table) {
+    Cursor* cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = table->num_rows;
+    cursor->end_of_table = true;
+
+    return cursor;
+}
+
+void* cursor_value(Cursor* cursor) {
+    uint32_t row_num = cursor->row_num;
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void *page = get_page(cursor->table->pager, page_num);
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
+}
+
+void cursor_advance(Cursor* cursor) {
+    cursor->row_num += 1;
+    if (cursor->row_num >= cursor->table->num_rows) {
+        cursor->end_of_table = true;
+    }
 }
 
 
@@ -122,8 +154,9 @@ ExecuteResult execute_insert(Statement* statement, Table* table) {
   }
 
   Row* row_to_insert = &(statement->row_to_insert);
+  Cursor* cursor = table_end(table);
 
-  serialize_row(row_to_insert, row_slot(table, table->num_rows));
+  serialize_row(row_to_insert, cursor_value(cursor));
   table->num_rows += 1;
 
   return EXECUTE_SUCCESS;
@@ -131,11 +164,17 @@ ExecuteResult execute_insert(Statement* statement, Table* table) {
 
 // EXECUTE SELECT FUNCTION
 ExecuteResult execute_select(Statement* statement, Table* table) {
+  Cursor* cursor = table_start(table);
   Row row;
-  for (uint32_t i = 0; i < table->num_rows; i++) {
-    deserialize_row(row_slot(table, i), &row);
-    print_row(&row);
-  }
+
+  while (!(cursor->end_of_table)) {
+     deserialize_row(cursor_value(cursor), &row);
+     print_row(&row);
+     cursor_advance(cursor);
+   }
+
+  free(cursor);
+  
   return EXECUTE_SUCCESS;
 }
 
