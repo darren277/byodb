fa5db12520b0eb769f94bfd0eb14d8dbe5c0d7b1
darren277 <darren277@yahoo.com>
2022-09-29 08:46:49 -0700

    Updated execute_insert() to use our new table_find() function and added duplicate key check.

--- src/inputbuffer.c
+++ src/inputbuffer.c
@@ -1,7 +1,7 @@
 
 typedef struct {char* buffer; size_t buffer_length; ssize_t input_length;} InputBuffer;
 
-typedef enum { EXECUTE_SUCCESS, EXECUTE_TABLE_FULL } ExecuteResult;
+typedef enum { EXECUTE_SUCCESS, EXECUTE_DUPLICATE_KEY, EXECUTE_TABLE_FULL } ExecuteResult;
 typedef enum { META_COMMAND_SUCCESS, META_COMMAND_UNRECOGNIZED_COMMAND } MetaCommandResult;
 typedef enum { PREPARE_SUCCESS, PREPARE_STRING_TOO_LONG, PREPARE_SYNTAX_ERROR, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
 typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
