3ca35b15a0fb1f00ba39fa5fd1a463c5e6466afd
darren277 <darren277@yahoo.com>
2022-09-29 09:53:40 -0700

    Added internal_node_insert() function.

--- src/leafnodeops.c
+++ src/leafnodeops.c
@@ -41,6 +41,45 @@ void create_new_root(Table* table, uint32_t right_child_page_num) {
 }
 
 
+
+void internal_node_insert(Table* table, uint32_t parent_page_num, uint32_t child_page_num) {
+  // Add a new child/key pair to parent that corresponds to child
+
+  void* parent = get_page(table->pager, parent_page_num);
+  void* child = get_page(table->pager, child_page_num);
+  uint32_t child_max_key = get_node_max_key(child);
+  uint32_t index = internal_node_find_child(parent, child_max_key);
+
+  uint32_t original_num_keys = *internal_node_num_keys(parent);
+  *internal_node_num_keys(parent) = original_num_keys + 1;
+
+  if (original_num_keys >= INTERNAL_NODE_MAX_CELLS) {
+    printf("Need to implement splitting internal node\n");
+    exit(EXIT_FAILURE);
+  }
+  uint32_t right_child_page_num = *internal_node_right_child(parent);
+  void* right_child = get_page(table->pager, right_child_page_num);
+
+  if (child_max_key > get_node_max_key(right_child)) {
+    /* Replace right child */
+    *internal_node_child(parent, original_num_keys) = right_child_page_num;
+    *internal_node_key(parent, original_num_keys) =
+        get_node_max_key(right_child);
+    *internal_node_right_child(parent) = child_page_num;
+  } else {
+    /* Make room for the new cell */
+    for (uint32_t i = original_num_keys; i > index; i--) {
+      void* destination = internal_node_cell(parent, i);
+      void* source = internal_node_cell(parent, i - 1);
+      memcpy(destination, source, INTERNAL_NODE_CELL_SIZE);
+    }
+    *internal_node_child(parent, index) = child_page_num;
+    *internal_node_key(parent, index) = child_max_key;
+  }
+}
+
+
+
 void leaf_node_split_and_insert(Cursor* cursor, uint32_t key, Row* value) {
   /* Create a new node and move half the cells over. Insert the new value in one of the two nodes. Update parent or create a new parent.  */
   void* old_node = get_page(cursor->table->pager, cursor->page_num);
