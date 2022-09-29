066bb92f2f68b478eef4242daa5d76fd3e7b61ee
darren277 <darren277@yahoo.com>
2022-09-29 09:08:40 -0700

    Added internal node layout consts and functions as well as root node getter and setter.

--- src/btree.c
+++ src/btree.c
@@ -28,6 +28,19 @@ const uint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
 const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
 const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
 
+/* Internal Node Header Layout */
+
+const uint32_t INTERNAL_NODE_NUM_KEYS_SIZE = sizeof(uint32_t);
+const uint32_t INTERNAL_NODE_NUM_KEYS_OFFSET = COMMON_NODE_HEADER_SIZE;
+const uint32_t INTERNAL_NODE_RIGHT_CHILD_SIZE = sizeof(uint32_t);
+const uint32_t INTERNAL_NODE_RIGHT_CHILD_OFFSET = INTERNAL_NODE_NUM_KEYS_OFFSET + INTERNAL_NODE_NUM_KEYS_SIZE;
+const uint32_t INTERNAL_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + INTERNAL_NODE_NUM_KEYS_SIZE + INTERNAL_NODE_RIGHT_CHILD_SIZE;
+
+/* Internal Node Body Layout */
+
+const uint32_t INTERNAL_NODE_KEY_SIZE = sizeof(uint32_t);
+const uint32_t INTERNAL_NODE_CHILD_SIZE = sizeof(uint32_t);
+const uint32_t INTERNAL_NODE_CELL_SIZE = INTERNAL_NODE_CHILD_SIZE + INTERNAL_NODE_KEY_SIZE;
 
 
 /* LEAF NODE ACCESS FUNCTIONS */
@@ -36,6 +49,23 @@ uint32_t* leaf_node_num_cells(void* node) {return node + LEAF_NODE_NUM_CELLS_OFF
 void* leaf_node_cell(void* node, uint32_t cell_num) {return node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE;}
 uint32_t* leaf_node_key(void* node, uint32_t cell_num) {return leaf_node_cell(node, cell_num);}
 void* leaf_node_value(void* node, uint32_t cell_num) {return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;}
+
+
+uint32_t* internal_node_num_keys(void* node) {return node + INTERNAL_NODE_NUM_KEYS_OFFSET;}
+uint32_t* internal_node_right_child(void* node) {return node + INTERNAL_NODE_RIGHT_CHILD_OFFSET;}
+uint32_t* internal_node_cell(void* node, uint32_t cell_num) {return node + INTERNAL_NODE_HEADER_SIZE + cell_num * INTERNAL_NODE_CELL_SIZE;}
+
+
+
+bool is_node_root(void* node) {
+  uint8_t value = *((uint8_t*)(node + IS_ROOT_OFFSET));
+  return (bool)value;
+}
+
+void set_node_root(void* node, bool is_root) {
+  uint8_t value = is_root;
+  *((uint8_t*)(node + IS_ROOT_OFFSET)) = value;
+}
 
 
 NodeType get_node_type(void* node) {
@@ -51,5 +81,12 @@ void set_node_type(void* node, NodeType type) {
 
 void initialize_leaf_node(void* node) {
     set_node_type(node, NODE_LEAF);
+    set_node_root(node, false);
     *leaf_node_num_cells(node) = 0;
+}
+
+void initialize_internal_node(void* node) {
+  set_node_type(node, NODE_INTERNAL);
+  set_node_root(node, false);
+  *internal_node_num_keys(node) = 0;
 }
