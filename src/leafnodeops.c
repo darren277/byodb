7019721f390ef0593804914cd19c5d032a4b8621
darren277 <darren277@yahoo.com>
2022-09-29 09:09:56 -0700

    Added create_new_root() and some associated helper functions.

--- src/leafnodeops.c
+++ src/leafnodeops.c
@@ -1,4 +1,61 @@
 
+uint32_t* internal_node_child(void* node, uint32_t child_num) {
+  uint32_t num_keys = *internal_node_num_keys(node);
+  if (child_num > num_keys) {
+    printf("Tried to access child_num %d > num_keys %d\n", child_num, num_keys);
+    exit(EXIT_FAILURE);
+  } else if (child_num == num_keys) {
+    return internal_node_right_child(node);
+  } else {
+    return internal_node_cell(node, child_num);
+  }
+}
+
+uint32_t* internal_node_key(void* node, uint32_t key_num) {return internal_node_cell(node, key_num) + INTERNAL_NODE_CHILD_SIZE;}
+
+
+uint32_t get_node_max_key(void* node) {
+  switch (get_node_type(node)) {
+    case NODE_INTERNAL:
+      return *internal_node_key(node, *internal_node_num_keys(node) - 1);
+    case NODE_LEAF:
+      return *leaf_node_key(node, *leaf_node_num_cells(node) - 1);
+  }
+}
+
+
+// Until we start recycling free pages, new pages will always go onto the end of the database file
+uint32_t get_unused_page_num(Pager* pager) { return pager->num_pages; }
+
+
+const uint32_t LEAF_NODE_RIGHT_SPLIT_COUNT = (LEAF_NODE_MAX_CELLS + 1) / 2;
+const uint32_t LEAF_NODE_LEFT_SPLIT_COUNT = (LEAF_NODE_MAX_CELLS + 1) - LEAF_NODE_RIGHT_SPLIT_COUNT;
+
+
+void create_new_root(Table* table, uint32_t right_child_page_num) {
+  // Handle splitting the root. Old root copied to new page, becomes left child. Address of right child passed in.
+  // Re-initialize root page to contain the new root node. New root node points to two children.
+
+  void* root = get_page(table->pager, table->root_page_num);
+  void* right_child = get_page(table->pager, right_child_page_num);
+  uint32_t left_child_page_num = get_unused_page_num(table->pager);
+  void* left_child = get_page(table->pager, left_child_page_num);
+
+  /* Left child has data copied from old root */
+  memcpy(left_child, root, PAGE_SIZE);
+  set_node_root(left_child, false);
+
+  /* Root node is a new internal node with one key and two children */
+  initialize_internal_node(root);
+  set_node_root(root, true);
+  *internal_node_num_keys(root) = 1;
+  *internal_node_child(root, 0) = left_child_page_num;
+  uint32_t left_child_max_key = get_node_max_key(left_child);
+  *internal_node_key(root, 0) = left_child_max_key;
+  *internal_node_right_child(root) = right_child_page_num;
+}
+
+
 void leaf_node_split_and_insert(Cursor* cursor, uint32_t key, Row* value) {
   /* Create a new node and move half the cells over. Insert the new value in one of the two nodes. Update parent or create a new parent.  */
   void* old_node = get_page(cursor->table->pager, cursor->page_num);
